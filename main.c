04d21d097344f2e6136c5d46aed48b2739398414
johnbodoh <john.joseph.bodoh@gmail.com>
2021-08-16 18:44:41 -0400

    Added instruction printing function

--- main.c
+++ main.c
@@ -5,9 +5,30 @@
 #include <stdint.h>
 #include <sys/stat.h>
 
+/*
+Instructions in 8080 assembly use seven types of parameters:
+NO_PARAM (no parameter), example: NOP
+S_REG (standalone register), example: INR A
+REG_8BIT (8-bit value to register), example: MVI B,D8
+REG_16BIT (16-bit value or address to register), example: LXI D,D16
+S_8BIT (standalone 8-bit value), example: OUT D8
+S_16BIT (standalone 16-bit value or register), example: JP adr
+*/
+
+typedef enum {
+    NO_PARAM,
+    S_REG,
+    REG_8BIT,
+    REG_16BIT,
+    REG_REG,
+    S_8BIT,
+    S_16BIT
+} InstParam;
+
 // void fillBuffer(FILE *input, int8_t buffer, int size);
 uint8_t *fillBuffer(FILE *input, int size);
 int findSize(FILE *input);
+int printInstruction(uint8_t *buffer, int location, int instSize, char *instName, char *reg1, char *reg2, InstParam parameter);
 void readBuffer(uint8_t *buffer, int size);
 
 int main(int argc, char *argv[])
@@ -61,6 +82,64 @@ int findSize(FILE *input)
     rewind(input);
     return size;
 }
+// printInstruction takes pointer to current location in file, integer of current location in file, integer of size of instruction in bytes, string of name of instruction, strings of registers or number parameters of instruction, and the type of parameter as defined above
+// printInstruction returns current value of location looping variable before next increment
+int printInstruction(uint8_t *buffer, int location, int instSize, char *instName, char *reg1, char *reg2, InstParam parameter)
+{
+    int i = 0;
+    printf("%04x ", location); // Print current location in file
+    for(; i < instSize; i++) // Print bytes of instruction
+    {
+        printf("%02x ", buffer[i]);
+    }
+    for(i = 0; i < (3 - instSize)*3; i++) // Print additional padding spaces if instruction is 1 or 2 bytes
+    {
+        putchar(' ');
+    }
+    printf("%s", instName); // Print instruction name
+    if(strlen(instName) < 4) // Print additional padding spaces if instruction name is less than 4 characters
+    {
+        for(i = 0; i < (4 - strlen(instName)); i++)
+        {
+            putchar(' ');
+        }
+    }
+    if(parameter != NO_PARAM)
+    {
+        printf("   "); // If instruction has parameter, print minimum number of spaces between instruction name and instruction parameter
+    }
+    switch(parameter) // Handle all parameter cases
+    {
+    case NO_PARAM: // Print nothing for no parameter
+        break;
+    case S_REG:
+        printf("%s", reg1); // Print register string
+        break;
+    case REG_8BIT:
+        printf("%s,$%02x", reg1, buffer[1]); // Print register string and 8-bit immediate value
+        location++;
+        buffer++;
+        break;
+    case REG_16BIT:
+        printf("%s,$%02x02x", reg1, buffer[2], buffer[1]); // Print register string and 16-bit little endian immediate value
+        location += 2;
+        buffer += 2;
+        break;
+    case REG_REG:
+        printf("%s,%s", reg1, reg2); // Print both register strings
+        break;
+    case S_8BIT:
+        printf("$%02x", buffer[1]); // Print 8-bit immediate value
+        location++;
+        buffer++;
+    case S_16BIT:
+        printf("$%02x%02x", buffer[2], buffer[1]); //Print little endian 16-bit immediate value
+        location += 2;
+        buffer += 2;
+    }
+    putchar('\n');
+    return(location);
+}
 void readBuffer(uint8_t *buffer, int size)
 {
     static uint8_t incIns1 = 0;
@@ -70,154 +149,15 @@ void readBuffer(uint8_t *buffer, int size)
     char inst[5];
     int i = 0;
 
-    for(; i < size; i++)
+    for(; i < size; i++, buffer++)
     {
         switch(*buffer)
         {
-        case 0x00:
-            printf("%04x %02x       NOP\n", i, *buffer);
+        case 0x00: // NOP
+            i = printInstruction(buffer, i, 1, "NOP", "", "", NO_PARAM);
             break;
-        case 0x01:
-            printf("%04x %02x %02x %02x LXI    B,$%02x%02x\n", i, buffer[0], buffer[1], buffer[2], buffer[2], buffer[1]);
-            buffer += 2;
-            i += 2;
-            break;
-        case 0x02:
-            printf("%04x %02x       STAX   B\n", i, *buffer);
-            break;
-        case 0x03:
-            printf("%04x %02x       INX    B\n", i, *buffer);
-            break;
-        case 0x04:
-            printf("%04x %02x       INR    B\n", i, *buffer);
-            break;
-        case 0x05:
-            printf("%04x %02x       DCR    B\n", i, *buffer);
-            break;
-        case 0x06:
-            printf("%04x %02x %02x    MVI    B,$%02x\n", i, buffer[0], buffer[1], buffer[1]);
-            buffer += 1;
-            i += 1;
-            break;
-        case 0x07:
-            printf("%04x %02x       RLC\n", i, *buffer);
-            break;
-        case 0x08:
-            printf("%04x %02x       --\n", i, *buffer);
-            break;
-        case 0x09:
-            printf("%04x %02x       DAD    B\n", i, *buffer);
-            break;
-        case 0x0a:
-            printf("%04x %02x       LDAX   B\n", i, *buffer);
-            break;
-        case 0x0b:
-            printf("%04x %02x       DCX    B\n", i, *buffer);
-            break;
-        case 0x0c:
-            printf("%04x %02x       INR    C\n", i, *buffer);
-            break;
-        case 0x0d:
-            printf("%04x %02x       DCR    C\n", i, *buffer);
-            break;
-        case 0x0e:
-            printf("%04x %02x %02x    MVI    C,$%02x\n", i, buffer[0], buffer[1], buffer[1]);
-            buffer += 1;
-            i += 1;
-            break;
-        case 0x0f:
-            printf("%04x %02x       RRC\n", i, *buffer);
-            break;
-        case 0x10:
-            printf("%04x %02x       --\n", i, *buffer);
-            break;
-        case 0x11:
-            printf("%04x %02x %02x %02x LXI    D,$%02x%02x\n", i, buffer[0], buffer[1], buffer[2], buffer[2], buffer[1]);
-            buffer += 2;
-            i += 2;
-            break;
-        case 0x12:
-            printf("%04x %02x       STAX   D\n", i, *buffer);
-            break;
-        case 0x13:
-            printf("%04x %02x       INX    D\n", i, *buffer);
-            break;
-        case 0x14:
-            printf("%04x %02x       INR    D\n", i, *buffer);
-            break;
-        case 0x15:
-            printf("%04x %02x       DCR    D\n", i, *buffer);
-            break;
-        case 0x16:
-            printf("%04x %02x %02x    MVI    D,$%02x\n", i, buffer[0], buffer[1], buffer[1]);
-            buffer += 1;
-            i += 1;
-            break;
-        case 0x17:
-            printf("%04x %02x       RAL\n", i, *buffer);
-            break;
-        case 0x18:
-            printf("%04x %02x       --\n", i, *buffer);
-            break;
-        case 0x19:
-            printf("%04x %02x       DAD    D\n", i, *buffer);
-            break;
-        case 0x1a:
-            printf("%04x %02x       LDAX   D\n", i, *buffer);
-            break;
-        case 0x1b:
-            printf("%04x %02x       DCX    D\n", i, *buffer);
-            break;
-        case 0x1c:
-            printf("%04x %02x       INR    E\n", i, *buffer);
-            break;
-        case 0x1d:
-            printf("%04x %02x       DCR    E\n", i, *buffer);
-            break;
-        case 0x1e:
-            printf("%04x %02x %02x    MVI    E,$%02x\n", i, buffer[0], buffer[1], buffer[1]);
-            buffer += 1;
-            i += 1;
-            break;
-        case 0x1f:
-            printf("%04x %02x       RAR\n", i, *buffer);
-            break;
-        case 0x20:
-            printf("%04x %02x       --\n", i, *buffer);
-            break;
-        case 0x21:
-            printf("%04x %02x %02x %02x LXI    H,$%02x%02x\n", i, buffer[0], buffer[1], buffer[2], buffer[2], buffer[1]);
-            buffer += 2;
-            i += 2;
-            break;
-        case 0x32:
-            printf("%04x %02x %02x %02x STA    $%02x%02x\n", i, buffer[0], buffer[1], buffer[2], buffer[2], buffer[1]);
-            buffer += 2;
-            i += 2;
-            break;
-        case 0x3e:
-            printf("%04x %02x %02x    MVI    A,$%02x\n", i, buffer[0], buffer[1], buffer[1]);
-            buffer += 1;
-            i += 1;
-            break;
-        case 0xc3:
-            printf("%04x %02x %02x %02x JMP    $%02x%02x\n", i, buffer[0], buffer[1], buffer[2], buffer[2], buffer[1]);
-            buffer += 2;
-            i += 2;
-            break;
-        case 0xc5:
-            printf("%04x %02x       PUSH   B\n", i, *buffer);
-            break;
-        case 0xd5:
-            printf("%04x %02x       PUSH   D\n", i, *buffer);
-            break;
-        case 0xe5:
-            printf("%04x %02x       PUSH   H\n", i, *buffer);
-            break;
-        case 0xf5:
-            printf("%04x %02x       PUSH   PSW\n", i, *buffer);
-            break;
+        default: // Undefined instructions
+            i = printInstruction(buffer, i, 1, "--", "", "", NO_PARAM);
         }
-        buffer++;
     }
 }
